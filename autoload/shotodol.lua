4925c80c8a542f8576b5680bfe58005b822b3218
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2015-01-19 23:40:43 +0600

    We fixed the quit command and we loaded a plain text

--- autoload/shotodol.lua
+++ autoload/shotodol.lua
@@ -5,6 +5,6 @@ function rehash()
 end
 
 function exten_vela_page_menu(x) 
-	local msg = "default <x href=\"velaxecute://ls\" label=Open></x><x href=\"velaxecute://history -b 99 \" label=Quit></x><x href=\"velafopen://about.vapp\" label=About></x>\n"
+	local msg = "default <x href=\"velaxecute://ls\" label=Open></x><x href=\"velaxecute://history -b 99 \" label=Quit></x><x href=\"velafopen://velapp/about.txt\" label=About></x>\n"
 	return(msg)
 end
